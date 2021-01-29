f0fea042c158e89ec8f6417f038ab0c3488576f7
NeXiDE <66691455+NeXiDE@users.noreply.github.com>
2021-01-29 18:17:38 +0100

    Update srvmain.js

--- srvmain.js
+++ srvmain.js
@@ -311,7 +311,7 @@ module.exports = class Serv {
                 
         //spawn lol
         broadcast("respawn", id, { //keep in mind that they are multple spawns. Just use a random mechanism on them
-            distanceScore: 5,
+            distanceScore: 256,
             ...(mapSpawns[Math.random() * mapSpawns.length | 0]),
             x: 0,
             y: 0,
